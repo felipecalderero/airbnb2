8003e31023a2e8d0cf4599d868e385d6e7aad707
Felipe Calderero <felipecalderero@gmail.com>
2024-04-09 17:27:10 +0200

    setup gh-pages

--- vite-project/src/pages/DashboardPage.jsx
+++ vite-project/src/pages/DashboardPage.jsx
@@ -3,7 +3,7 @@ import AddRental from "../components/AddRental";
 
 const DashboardPage = ({ deleteRental, addRental, rentalList }) => {
   return (
-    <div className="dashboar">
+    <>
       <div className="top-ctn">
         <h1>List of Rentals</h1>
         <AddRental rentalList={rentalList} addRental={addRental} />
